6b9914c75cf86d68d088cee54e36e8d2cd938b5e
fcalve4 <freddycalve@gmail.com>
2025-01-13 04:49:14 +0000

    added some comments

--- include/deck.h
+++ include/deck.h
@@ -1,17 +1,40 @@
 #ifndef DECK_H
 #define DECK_H
 
+/**
+ * @file: deck.h
+ * @author: fcalve4
+ * 
+ * This file contains the declarations for the Deck struct and its functions,
+ * as well as the Card struct.
+ */
 
-typedef struct {
-    int rank; // 1-13 (1 = Ace, 11 = Jack, 12 = Queen, 13 = King)
-} Card;
 
+/**
+ * @struct Deck
+ * @brief Represents a deck of cards.
+ * 
+ * The deck is implemented as a dynamically allocated array of cards. The deck (shoe)
+ * is initialized with a certain number of decks, and the cards are shuffled
+ * before being dealt. 
+ */
 typedef struct {
     Card *cards; // Dynamically allocated array of cards
     int num_cards; // Current number of cards
     int capacity; // Maximum capacity of the array
     int top;
 } Deck;
+
+/**
+ * @struct Card
+ * @brief Represents a single card in a deck.
+ * 
+ * The card is represented by its rank, which is an integer from 1 to 13.
+ * 10-13 represents face cards (10-value), while 1 represents an Ace.
+ */
+typedef struct {
+    int rank; // 1-13 (1 = Ace, 11 = Jack, 12 = Queen, 13 = King)
+} Card;
 
 // Deck functions
 void init_deck(Deck *deck, int numDecks);
